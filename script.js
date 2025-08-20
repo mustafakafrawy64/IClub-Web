41947972a43dd01fbfc6ccc6c3d5ec868dcea6fe
Mostafa <mustafa.kafrawy123@gmail.com>
2025-08-20 23:39:24 +0300

    third section

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
   document.getElementById("navLinks").classList.toggle("show");
 }
+
+const loadMoreBtn = document.getElementById('andMoreBtn');
+const hiddenSpeakers = document.querySelectorAll('.hidden');
+
+loadMoreBtn.addEventListener('click', () => {
+    hiddenSpeakers.forEach(element => {
+        element.classList.remove('hidden');
+    });
+
+    loadMoreBtn.style.display = 'none';
+});
